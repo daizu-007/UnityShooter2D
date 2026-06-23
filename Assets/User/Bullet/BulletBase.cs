20cadaed3bb16bd58cc27ce1f73d24c0e8ea7ecb
daizu-007 <128668088+daizu-007@users.noreply.github.com>
2026-06-23 19:29:55 +0900

    delete debug

--- Assets/User/Bullet/BulletBase.cs
+++ Assets/User/Bullet/BulletBase.cs
@@ -22,10 +22,8 @@ public class BulletBase : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("弾が何かに当たった");
         if (other.CompareTag("Player"))
         {
-            Debug.Log("弾がプレイヤーに当たった");
             PlayerController player = other.GetComponent<PlayerController>();
             if (player != null)
             {
